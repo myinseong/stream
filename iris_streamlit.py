51d53bb89d0e7c1347dab2bda6d7a098c4c59e42
myinseong <mynseong@naver.com>
2024-11-25 17:29:04 +0900

    Add files via upload

--- iris_streamlit.py
+++ iris_streamlit.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd 
-from sklearn.datasets import load_iris 
+import scikit_learn.datasets as load_iris 
 import matplotlib.pyplot as plt
 import streamlit as st
 
